1889dff162990b6580778efb3ada734306986e1b
Leonardo Augusto Schmitz-LEOAUG <leoaug@motorola.com>
2017-12-29 16:02:14 -0200

    Add cpf validation skeleton

--- validate_cpf_cnpj.js
+++ validate_cpf_cnpj.js
@@ -1,23 +1,34 @@
 /**
+ * Remove code formatting
+ * @param {string} code Code
+ * @returns {string} unformatted code
+ */
+var get_numbers = function(code){
+	numbers = ''
+	if (!code || code.length === 0)
+		return numbers
+	for (var i = 0; i < code.length; i++){
+		if (!isNaN(code[i])){
+			numbers += code[i];
+		}
+	}
+	return numbers;
+}
+
+/**
  * Validates CPF (Cadastro de Pessoa Física)
  *
- * @param {string} cpf CPF code
+ * @param {string} cpf CPF code, which can be formatted or not
  * @returns {bool} valid or invalid
  */
 var validate_cpf = function(cpf){
 	var valid = false;
-	if (!cpf || cpf.length === 0)
-		return valid;
-
-	cpf = cpf.trim();
-	if (cpf.length === 14){
-		cpf = cpf.replace( '/^\D+/g', '' );
-		console.log(cpf);
-	}
-	console.log(cpf);
-
-	for (var i = 0; i < cpf.length; i++){
-	}
+	cpf = get_numbers(cpf);
+	if (cpf.length != 10)
+		return false;
+	return true
 }
-validate_cpf()
-validate_cpf('009.613.760-66')
+
+console.log(validate_cpf());
+console.log(validate_cpf('0000000000'));
+console.log(validate_cpf('000.000.00-00'));
