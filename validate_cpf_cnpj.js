6af57ffe1db46903a20417d9505fcd8e5444e856
Leonardo Augusto Schmitz <laschmitz@gmail.com>
2017-12-29 23:02:33 -0200

    Add validate CPF logic

--- validate_cpf_cnpj.js
+++ validate_cpf_cnpj.js
@@ -1,15 +1,15 @@
 /**
  * Remove code formatting
  * @param {string} code Code
- * @returns {string} unformatted code
+ * @returns {list} list of digits
  */
 var get_numbers = function(code){
-	numbers = ''
+	numbers = []
 	if (!code || code.length === 0)
 		return numbers
 	for (var i = 0; i < code.length; i++){
 		if (!isNaN(code[i])){
-			numbers += code[i];
+			numbers.push(parseInt(code[i], 10));
 		}
 	}
 	return numbers;
@@ -24,11 +24,24 @@ var get_numbers = function(code){
 var validate_cpf = function(cpf){
 	var valid = false;
 	cpf = get_numbers(cpf);
-	if (cpf.length != 10)
+	if (cpf.length != 11)
 		return false;
-	return true
+
+	var digit1 = 0;
+	var digit2 = 0;
+	for (var i = 0; i < 9; i++){
+		digit1 = digit1 + cpf[i] * (10 - i);
+		digit2 = digit2 + cpf[i] * (11 - i);
+	}
+	digit1 = (digit1 % 11) > 1 ? 11 - (digit1 % 11) : 0;
+	digit2 = digit2 + digit1 * 2;
+	digit2 = (digit2 % 11) > 1 ? 11 - (digit2 % 11) : 0;
+
+	return digit2 == cpf[10] && digit1 == cpf[9];
 }
 
 console.log(validate_cpf());
-console.log(validate_cpf('0000000000'));
-console.log(validate_cpf('000.000.00-00'));
+console.log(validate_cpf(''));
+console.log(validate_cpf('123.456.789-09'));
+console.log(validate_cpf('843.667.254-28'));
+console.log(validate_cpf('111.444.777-35'));
