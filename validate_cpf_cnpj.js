46fb4139f276fa48c1211e9251acb69e2ca86af5
Leonardo Augusto Schmitz <laschmitz@gmail.com>
2017-12-30 00:43:11 -0200

    Add base for CNPJ validation

--- validate_cpf_cnpj.js
+++ validate_cpf_cnpj.js
@@ -4,9 +4,9 @@
  * @returns {list} list of digits
  */
 function getNumbers(code){
-	numbers = []
+	numbers = [];
 	if (!code || code.length === 0)
-		return numbers
+		return numbers;
 	for (var i = 0; i < code.length; i++){
 		if (!isNaN(code[i])){
 			numbers.push(parseInt(code[i], 10));
@@ -40,13 +40,49 @@ function getCPFCheckDigits(cpf){
  */
 function isCPFValid(cpf){
 	var valid = false;
+
 	cpf = getNumbers(cpf);
 	if (cpf.length != 11)
 		return false;
 
 	var checkdigits = getCPFCheckDigits(cpf);
-
 	return checkdigits[0] == cpf[9] && checkdigits[1] == cpf[10];
+}
+
+/**
+ * Get both CNPJ check digits.
+ * @param {string} cnpj CNPJ numbers only
+ * @returns {list} digit 1 and 2
+ */
+function getCNPJCheckDigits(cnpj){
+	var digit1 = 0;
+	var digit2 = 0;
+	for (var i = 0; i < 4; i++){
+		digit1 = digit1 + cnpj[i] * (5 - i);
+		digit2 = digit2 + cnpj[i] * (6 - i);
+	}
+	digit2 = digit2 + cnpj[4] * 2;
+	for (var i = 0; i < 8; i++){
+		digit1 = digit1 + cnpj[i + 4] * (9 - i);
+		digit2 = digit2 + cnpj[i + 5] * (9 - i);
+	}
+	digit1 = (digit1 % 11) > 1 ? 11 - (digit1 % 11) : 0;
+	digit2 = (digit2 % 11) > 1 ? 11 - (digit2 % 11) : 0;
+	return [digit1, digit2];
+}
+
+/**
+ * Validate CNPJ
+ */
+function isCNPJValid(cnpj){
+	var valid = false;
+
+	cnpj = getNumbers(cnpj);
+	if (cnpj.length != 14)
+		return false;
+	
+	var checkdigits = getCNPJCheckDigits(cnpj);
+	return checkdigits[0] == cnpj[12] && checkdigits[1] == cnpj[13];
 }
 
 console.log(isCPFValid());
@@ -54,3 +90,8 @@ console.log(isCPFValid(''));
 console.log(isCPFValid('123.456.789-09'));
 console.log(isCPFValid('843.667.254-28'));
 console.log(isCPFValid('111.444.777-35'));
+
+console.log(isCNPJValid());
+console.log(isCNPJValid(''));
+// console.log(isCNPJValid('33.000.167/0001-01'));
+console.log(isCNPJValid('11.444.777/0001-61'));
